044ec7415cded46a3a470ca76438bb4e2bb858ec
Lawrence <lawrencechan27@gmail.com>
2023-05-31 23:28:18 +1000

    Added IDs to list items and delete function

--- todo.js
+++ todo.js
@@ -13,29 +13,35 @@
 // define global array that holds list of items
 var listArray = [];
 
+// define global ID counter var to assign to each item
+var currentId = 0;
+
 // function to add item
 const submitField = function(){
     // create a new object and place inputField into the text property
     let inputItem = {
-        "id": 1,
+        "id": currentId,
         "text": document.querySelector('.inputField').value
     }
+    // increment currentId for the next entry
+    currentId++;
     // add object to listArray
     listArray.push(inputItem);
     // clear typeArea
     document.querySelector('.inputField').value = "";
     // run update function
     updateList();
-    // for testing purposes
-    console.table(listArray)
 }
 
 // function to remove item
-// [x] button on div, on click:
-// find parent ID
-// loop through listArray and look for matching ID
-// remove item from array
-// run populate function
+const deleteItem = function(elem){
+    // assign ID of the clicked button's parent div to a var
+    let deletionId = elem.parentNode.id;
+    // use filter to remove object that matches deletionId
+    listArray = listArray.filter(item => item.id != deletionId);
+    // run update function
+    updateList();
+}
 
 // function to update the list
 const updateList = function(){
@@ -48,14 +54,11 @@ const updateList = function(){
     for (item of listArray){
         // append HTML to div.listContanier
         let listItem = `
-        <div class="listItem" id="1">
-        <div class="listText">${item.text}</div>
-        <div class="deleteBtn">X</div></div>
+        <div class="listItem" id="${item.id}">
+            <div class="listText">${item.text}</div>
+            <div class="deleteBtn" onclick="deleteItem(this)">X</div>
+        </div>
         `;
         listContainer.innerHTML += listItem;
     }
 }
-
-// function to populate/update the list
-// loop through listArray
-// for each item in Array, append listItem to a div, assign ID to div ID and string to contents
