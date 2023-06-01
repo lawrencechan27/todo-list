9daf6e87b3c151eb3a8af8ba6c2eefd49346cd59
Lawrence <lawrencechan27@gmail.com>
2023-06-01 19:51:02 +1000

    Added ability to edit items as well as sort by new/old/alpha

--- todo.js
+++ todo.js
@@ -1,22 +1,47 @@
 // define global array that holds list of items
 var listArray = [];
 
-// define global ID counter var to assign to each item
-var currentId = 0;
-
 // function to add item
 const submitField = function(){
     // create a new object and place inputField into the text property
     let inputItem = {
-        "id": currentId,
+        "id": Date.now(),
         "text": document.querySelector('.inputField').value
     }
-    // increment currentId for the next entry
-    currentId++;
     // add object to listArray
     listArray.push(inputItem);
     // clear typeArea
     document.querySelector('.inputField').value = "";
+    // run update function
+    updateList();
+}
+
+// function to edit item
+const editItem = function(elem){
+    // hide the edit button
+    elem.style.display = 'none';
+    // unhide the save button by changing display:none to display:block
+    let saveBtn = elem.nextElementSibling;
+    saveBtn.style.display = 'block';
+    // apply editable attribute to the text field, and make it green
+    let textField = elem.previousElementSibling;
+    textField.contentEditable = true;
+    textField.classList.add('beingEdited');
+}
+
+// function to save edit
+const saveItem = function(elem){
+    // assign textField as first child of parent div
+    let textField = elem.parentNode.children[0];
+    // remove editable attribute and remove being edited class
+    textField.contentEditable = false;
+    textField.classList.remove('beingEdited');
+    // target the relevant object by matching the ID
+    let thisId = elem.parentNode.id;
+    let targetItem = listArray.find(obj => obj.id == thisId)
+    // change the target objects text to whatever was in editable field
+    let newText = elem.parentNode.children[0].innerHTML;
+    targetItem.text = newText;
     // run update function
     updateList();
 }
@@ -44,9 +69,36 @@ const updateList = function(){
         let listItem = `
         <div class="listItem" id="${item.id}">
             <div class="listText">${item.text}</div>
+            <div class="editBtn" onclick="editItem(this)">✎</div>
+            <div class="saveBtn" onclick="saveItem(this)" style="display:none;">💾</div>
             <div class="deleteBtn" onclick="deleteItem(this)">X</div>
         </div>
         `;
         listContainer.innerHTML += listItem;
     }
+    console.table(listArray);
+}
+
+// sort by newest first
+const sortNew = function(){
+    listArray.sort(
+        (a,b) => a.id > b.id ? -1 : 1
+    )
+    updateList();
+}
+
+// sort by oldest first
+const sortOld = function(){
+    listArray.sort(
+        (a,b) => a.id > b.id ? 1 : -1
+    )
+    updateList();
+}
+
+// sort by text first
+const sortAlpha = function(){
+    listArray.sort(
+        (a,b) => a.text > b.text ? 1 : -1
+    )
+    updateList();
 }
